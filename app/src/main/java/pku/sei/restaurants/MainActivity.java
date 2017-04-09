e7dc6c0753e8f2a3167bb0a7e7843c7209479993
Actasclown <gshe009@163.com>
2017-04-09 22:42:02 +0800

    Test.sout.

--- app/src/main/java/pku/sei/restaurants/MainActivity.java
+++ app/src/main/java/pku/sei/restaurants/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //String test;
+        System.out.println("TEST");
 
     }
     // This is a test!!!
